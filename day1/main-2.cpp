eaca82cbdbd9e9420de73c2c0bd04c7c896c8ba5
AHolubov <andriy.holubov@gmail.com>
2022-07-18 08:35:19 +0300

    Add separate solutions for parts 1 and 2 of day 1

--- day1/main-2.cpp
+++ day1/main-2.cpp
@@ -9,16 +9,24 @@ int main()
     std::ifstream file("input.txt");
     std::string line;
     int increaseCounter = 0;
+    std::vector<int> measurement;
     if (file.is_open())
     {
         std::getline (file, line);
-        int number = std::stoi(line);
+        measurement.push_back(std::stoi(line));
+        std::getline (file, line);
+        measurement.push_back(std::stoi(line));
+        std::getline (file, line);
+        measurement.push_back(std::stoi(line));
+        int number = std::accumulate(measurement.begin(), measurement.end(), 0);
         while (file)
         {
             std::getline (file, line);
             if (!line.empty())
             {
-                int nextNumber = std::stoi(line);
+                measurement.push_back(std::stoi(line));
+                measurement.erase(measurement.begin());
+                int nextNumber = std::accumulate(measurement.begin(), measurement.end(), 0);
                 if (nextNumber > number)
                 {
                     increaseCounter++;
